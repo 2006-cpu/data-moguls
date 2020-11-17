3ff5917a32105cefc18f21b1f5a70ecfdb06fd1a
lionl <lionllopez@gmail.com>
2020-11-17 11:06:00 -0800

    feat(poducts-table) Create products table

--- db/init_db.js
+++ db/init_db.js
@@ -9,13 +9,37 @@ async function buildTables() {
     client.connect();
 
     // drop tables in correct order
+    console.log('Dropping All Tables..');
+    try {
+      await client.query(`
+        DROP TABLE IF EXISTS products;
+      `);
+    } catch (error) {
+      throw error;
+    };
 
     // build tables in correct order
+    console.log('Starting to build tables...');
+    try {
+      await client.query(`
+        CREATE TABLE products (
+          id SERIAL PRIMARY KEY,
+          name VARCHAR(255) NOT NULL,
+          description TEXT NOT NULL,
+          price INTEGER NOT NULL,
+          "imageURL" VARCHAR(255) DEFAULT 'imageUrl goes here',
+          "inStock" BOOLEAN NOT NULL DEFAULT false,
+          category VARCHAR(255) NOT NULL
+        );
+      `);
+    } catch (error) {
+      throw error;
+    };
 
   } catch (error) {
     throw error;
-  }
-}
+  };
+};
 
 async function populateInitialData() {
   try {
