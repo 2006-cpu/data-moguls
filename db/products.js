eb0a33a582021682bb873896ba7c3ea16cac671c
lionl <lionllopez@gmail.com>
2020-11-17 18:01:34 -0800

    Added getAllProducts function.

--- db/products.js
+++ db/products.js
@@ -12,3 +12,15 @@ async function getProductById(id) {
         throw error;
     };
 };
+
+async function getAllProducts(){
+    try {
+        const {rows: products} = await client.query(`
+            SELECT * FROM products;
+        `);
+
+        return products;
+    } catch (error) {
+        throw error;
+    };
+};
