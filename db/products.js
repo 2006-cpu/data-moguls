5df5a33959dd6e09e01adf4ef21d60cb7470730e
lionl <lionllopez@gmail.com>
2020-11-17 18:08:30 -0800

    Added createProduct function.

--- db/products.js
+++ db/products.js
@@ -24,3 +24,17 @@ async function getAllProducts(){
         throw error;
     };
 };
+
+async function createProduct({name, description, price, imageURL, inStock, category}){
+    try {
+        const {rows: [product]} = await client.query(`
+            INSERT INTO products(name, description, price, "imageURL", "inStock", category)
+            VALUES ($1, $2, $3, $4, $5, $6)
+            RETURNING *;
+        `, [name, description, price, imageURL, inStock, category]);
+
+        return product;
+    } catch (error) {
+        throw error;
+    };
+};
