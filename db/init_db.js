a78291096a63dae02ac19f5c2ecdafd77cc3cf02
Ajrelerford <ajrelerford@sbcglobal.net>
2020-11-18 09:45:58 -0800

    feat(order_products table): added order products table

--- db/init_db.js
+++ db/init_db.js
@@ -31,6 +31,13 @@ async function buildTables() {
           "inStock" BOOLEAN NOT NULL DEFAULT false,
           category VARCHAR(255) NOT NULL
         );
+        CREATE TABLE order_products (
+          id SERIAL PRIMARY KEY,
+          "productId" INTEGER REFERENCES products(id),
+          "orderId" INTEGER REFERENCES orders(id),
+          price INTEGER NOT NULL,
+          quantity INTEGER NOT NULL DEFAULT 0
+        );
       `);
     } catch (error) {
       throw error;
