6e0010336f0f4a79cea9316409e4aae297bc0881
lionl <lionllopez@gmail.com>
2020-11-18 17:39:45 -0800

    Added product route to frontend

--- src/api/index.js
+++ src/api/index.js
@@ -9,3 +9,13 @@ export async function getSomething() {
   }
 }
 
+export async function getProductById(id){
+  try {
+    const { data } = await axios.get(`/api/products/${id}`);
+
+    return data;
+  } catch (error) {
+    throw error;
+  };
+};
+
--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import { 
+  Route,
+  Switch,
+  BrowserRouter as Router
+} from 'react-router-dom';
+
 import {
-  App
+  Product
 } from './components';
+
+const App = () => {
+
+  return <Router>
+    <Switch>
+      <Route path="/product/:productId">
+        <Product />
+      </Route>
+    </Switch>
+  </Router>
+};
 
 ReactDOM.render(
   <App />,
