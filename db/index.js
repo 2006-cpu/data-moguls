6f1f6848bdb5ba721a21c0ddfee9f0949f3fd867
dennisCastro <denniscastro@macbook-pro.lan>
2020-11-21 17:22:58 -0800

    Modified package.json and db/index.js

--- db/index.js
+++ db/index.js
@@ -1,6 +1,6 @@
 // Connect to DB
 const { Client } = require('pg');
-const DB_NAME = 'localhost:5432/data-moguls'
+const DB_NAME = 'localhost:5432/data-moguls';
 const DB_URL = process.env.DATABASE_URL || `postgres://${DB_NAME}`;
 const client = new Client(DB_URL);
 
