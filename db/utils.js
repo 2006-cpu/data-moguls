6688caf9b422f548245c95c23802e3a42792c796
Frank Silva <frankjoesilva@gmail.com>
2020-11-23 21:29:52 -0800

    utils file added to db for auth middleware

---
+++ db/utils.js
@@ -0,0 +1,14 @@
+const requireUser = (req, res, next) => {
+    if (!req.user) {
+        next({
+            name: "MissingUserError",
+            message: "You must be logged in to perform this action"
+        });
+    } else {
+        next()
+    }
+}
+
+module.exports = {
+    requireUser,
+}
