110466b0583a3a393d1c4e1de49e7592e713b1fd
dennisCastro <denniscastro@macbook-pro.lan>
2020-11-23 20:47:59 -0800

    All Products Images Fix

--- src/components/Allproducts.js
+++ src/components/Allproducts.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { getAllProducts } from '../api';
 import { NavLink } from 'react-router-dom';
 import './Components.css';
-import Image001 from './assets/image001.jpeg';
+// import Image001 from '/assets/image001.jpeg';
 
 export default function Allproducts() {
 
@@ -24,54 +24,13 @@ export default function Allproducts() {
 
   return (
     <div className='all-products'>
-
-      <div className='product-card'>
-        <h3>Dummy Name</h3>
-        <NavLink to='/product:productId'><img className='thumbnail' src={Image001} /></NavLink>
-        <p>Dummy Category</p>
-        <p>Price: $4.99</p>
-        <p>In stock</p>
-      </div>
-
-      <div className='product-card'>
-      <h3>Dummy Name</h3>
-        <NavLink to='/product:productId'><img className='thumbnail' src={Image001} /></NavLink>
-        <p>Dummy Category</p>
-        <p>Price: $4.99</p>
-        <p>In stock</p>
-      </div>
-
-      <div className='product-card'>
-      <h3>Dummy Name</h3>
-        <NavLink to='/product:productId'><img className='thumbnail' src={Image001} /></NavLink>
-        <p>Dummy Category</p>
-        <p>Price: $4.99</p>
-        <p>In stock</p>
-      </div>
-
-      <div className='product-card'>
-      <h3>Dummy Name</h3>
-        <NavLink to='/product:productId'><img className='thumbnail' src={Image001} /></NavLink>
-        <p>Dummy Category</p>
-        <p>Price: $4.99</p>
-        <p>In stock</p>
-      </div>
-
-      <div className='product-card'>
-      <h3>Dummy Name</h3>
-        <NavLink to='/product:productId'><img className='thumbnail' src={Image001} /></NavLink>
-        <p>Dummy Category</p>
-        <p>Price: $4.99</p>
-        <p>In stock</p>
-      </div>
-
       {products.map(({ id, name, description, price, imageURL, inStock, category }) => (
         <div key={id} className='product-card'>
-          <h3>{name}</h3>
           <NavLink to='/product:productId'><img className='thumbnail' src={imageURL} /></NavLink>
+          <h3>{name}</h3>
           <p>{category}</p>
-          <p>Price: {price}</p>
-          <p>{inStock}</p>
+          <p>Price: ${price * .01}</p>
+          <p style={{color: '#FF6666'}}>{inStock ? 'In Stock!' : 'Out of Stock'}</p>
         </div>
       ))}
     </div>
