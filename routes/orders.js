3e43496f97686d918843fd26923c79fc676882f6
Frank Silva <frankjoesilva@gmail.com>
2020-11-29 15:25:34 -0800

    working on orders route and orders front-end

--- routes/orders.js
+++ routes/orders.js
@@ -1,8 +1,7 @@
 const express = require('express');
-const jwt = require('jsonwebtoken')
-const { JWT_SECRET } = process.env
 const ordersRouter = express.Router();
-const { getOrderByUser } = require("../db/orders")
+const { getOrderByUser, getCartByUser, createOrder } = require("../db/orders")
+const { requireUser } = require('../db/utils')
 
 ordersRouter.get('/', async (req, res, next) => {
     const { username } = req.user
@@ -14,3 +13,35 @@ ordersRouter.get('/', async (req, res, next) => {
         console.error(error)
     }
 })
+
+ordersRouter.get('/cart', async (req, res, next) => {
+    const { username } = req.user
+    try {
+
+    } catch (error) {
+        console.error(error)
+    }
+})
+
+ordersRouter.get('/users/:userId/orders', requireUser, async (req, res, next) => {
+    try {
+        const { username } = req.params
+        if (req.user.username === username) {
+            const userOrders = await getOrderByUser({ username })
+            return res.send(userOrders)
+        }
+    } catch (error) {
+        next(error)
+    }
+})
+
+ordersRouter.post('/', async (req, res, next) => {
+    const { status } = req.params
+    try {
+
+        const createAnOrder = await createOrder({ status, userId })
+
+    } catch (error) {
+        console.error(error)
+    }
+});
