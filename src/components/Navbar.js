61b804ad365997d10c8ca58331297f19d57a2e11
Ajrelerford <ajrelerford@sbcglobal.net>
2020-11-30 20:02:38 -0800

    fixing

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,13 +8,8 @@ export default function Navbar() {
     <div className='navbar'>
       <NavLink to='/' className='button'>HOME</NavLink>
       <NavLink to='/products' className='button'>SHOP PRODUCTS</NavLink>
-<<<<<<< HEAD
       <NavLink to='/login' className='button'>LOGIN/SIGNUP</NavLink>
       <NavLink to='/orders' className='button'>ORDERS</NavLink>
-=======
-      <NavLink to='/login' className='button'>LOGIN</NavLink>
-      <NavLink to='/orders' className='button'>VIEW CART</NavLink>
->>>>>>> 499b0928c11ba065a10fd13154369d808bd6a661
       <Search />
     </div>
   );
