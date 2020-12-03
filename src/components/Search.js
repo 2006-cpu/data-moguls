09aac4fb67ea59aaa474fef8947f40625010904b
lionl <lionllopez@gmail.com>
2020-12-02 16:38:19 -0800

    Prevent default on search.

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,11 +3,16 @@ import { NavLink } from 'react-router-dom';
 import './Components.css';
 
 export default function Search() {
+
+  const handleClick = (event) => {
+    event.preventDefault();
+  };
+
   return (
     <div className='search'>
       <form className="search-form" action="/search">
         <input type="text" name="search-term" placeholder="SEARCH" />
-        <button id='search-button'>SEARCH</button>
+        <button id='search-button' onClick={ handleClick }>SEARCH</button>
     </form>
     </div>
   );
