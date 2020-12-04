9707c2453f11830aee5c3afa5b6ee1771e575406
Frank Silva <frankjoesilva@gmail.com>
2020-12-03 18:28:19 -0800

    working on testing routes

--- src/components/Orders.js
+++ src/components/Orders.js
@@ -1,33 +1,33 @@
-import React from 'react';
-import { getAllOrders } from '../api'
-import './Components.css';
+// import React from 'react';
+// import { getAllOrders } from '../api'
+// import './Components.css';
 
-const Order = ({
-  user,
-}) => {
-  let { orderId } = useParams();
-  const [order, setOrder] = useState([])
-  // const [status, setStatus] = useState([])
+// const Order = ({
+//   user,
+// }) => {
+//   let { orderId } = useParams();
+//   const [order, setOrder] = useState([])
+//   // const [status, setStatus] = useState([])
 
-  const fetchOrder = async () => {
-    try {
-      const orderFetched = await getAllOrders(orderId)
-      setOrder(orderFetched)
-    } catch (error) {
-      throw error
-    }
-  }
+//   const fetchOrder = async () => {
+//     try {
+//       const orderFetched = await getAllOrders(orderId)
+//       setOrder(orderFetched)
+//     } catch (error) {
+//       throw error
+//     }
+//   }
 
-  useEffect(() => {
-    fetchOrder(user)
-  }, [user]);
+//   useEffect(() => {
+//     fetchOrder(user)
+//   }, [user]);
 
-  return (
-    <div className='orders'>
-      <div key={order.id} className='order-card'></div>
-      <h3>{order.status}</h3>
-      <h3>{order.dataPlaced}</h3>
-    </div>
-  );
-};
-export default Order
+//   return (
+//     <div className='orders'>
+//       <div key={order.id} className='order-card'></div>
+//       <h3>{order.status}</h3>
+//       <h3>{order.dataPlaced}</h3>
+//     </div>
+//   );
+// };
+// export default Order
--- src/components/singleOrder.js
+++ src/components/singleOrder.js
@@ -1,39 +1,39 @@
-import React, { useState, useEffect } from 'react';
+// import React, { useState, useEffect } from 'react';
 
-import { useParams } from "react-router";
-import { getOrderById } from '../api';
-import { NavLink } from 'react-router-dom';
+// import { useParams } from "react-router";
+// import { getOrderById } from '../api';
+// import { NavLink } from 'react-router-dom';
 
-const SingleOrder = () => {
-    let { orderId } = useParams();
-    const [order, setOrder] = useState([])
+// const SingleOrder = () => {
+//     let { orderId } = useParams();
+//     const [order, setOrder] = useState([])
 
 
 
-    const fetchOrder = async () => {
-        try {
-            const orderFetched = await getOrderById(orderId);
+//     const fetchOrder = async () => {
+//         try {
+//             const orderFetched = await getOrderById(orderId);
 
-            setOrder(orderFetched);
+//             setOrder(orderFetched);
 
-        } catch (error) {
-            console.error(error)
-        }
-    }
+//         } catch (error) {
+//             console.error(error)
+//         }
+//     }
 
-    useEffect(() => {
-        fetchOrder();
-    }, []);
+//     useEffect(() => {
+//         fetchOrder();
+//     }, []);
 
-    return (<>
-        <div className='orders'>
-            <div key={order.id} className='order-card'></div>
-            <NavLink to={`/order/${id}`}></NavLink>
-            <h3>{order.status}</h3>
-            <h3>{order.dataPlaced}</h3>
-        </div>
-    </>)
-};
+//     return (<>
+//         <div className='orders'>
+//             <div key={order.id} className='order-card'></div>
+//             <NavLink to={`/order/${id}`}></NavLink>
+//             <h3>{order.status}</h3>
+//             <h3>{order.dataPlaced}</h3>
+//         </div>
+//     </>)
+// };
 
-export default SingleOrder;
+// export default SingleOrder;
 
