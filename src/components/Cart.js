55a57f6ac6bca219bb35f518671f004ab978bfdb
lionl <lionllopez@gmail.com>
2020-12-06 23:00:41 -0800

    Started Cart

---
+++ src/components/Cart.js
@@ -0,0 +1,9 @@
+import React, { useState } from 'react';
+import './Styles.css';
+
+export default function Cart() {
+
+    return <div className='orders'>
+        <h1>THIS IS THE CART IN PROGRESS</h1>
+    </div>
+};
