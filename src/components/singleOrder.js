2ecc024cbc6d85bfa2ffdb2faed054a43108fffd
Frank Silva <frankjoesilva@gmail.com>
2020-12-07 17:19:13 -0800

    merge conflicts

--- src/components/singleOrder.js
+++ src/components/singleOrder.js
@@ -1,38 +1,71 @@
-// import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from "react-router";
+import { getOrderById } from '../api';
+import './Styles.css';
 
-// import { useParams } from "react-router";
-// import { getOrderById } from '../api';
-// import { NavLink } from 'react-router-dom';
-// import './Styles.css';
+export default function Singleorder({ cart }) {
+    let { orderId } = useParams();
 
-// export default function singleOrder() {
-//     const [order, setOrder] = useState([])
-//     let { orderId } = useParams();
+    const displayCart = () => {
+        let total = 0;
 
+        return <div className='cart'>
+            <h1>Cart:</h1>
+            {cart.products.map(product => {
+                total = product.totalProductPrice + total;
+                return <div key={product.id}>
+                    <div className='product-card'><img className='thumbnail' src={product.imageURL} />
+                        <h3>{product.name}</h3>
+                        <p>Quantity: {product.quantity}</p>
+                        <p>Price: ${product.totalProductPrice * .01}</p>
+                    </div>
+                </div>
+            })}
+            <br />
+            <h2>Total:  ${(total * .01).toFixed(2)}</h2>
+            <button className="button" id="payButton">Pay now!</button>
+        </div>
+    };
 
-//     const fetchOrder = async () => {
-//         try {
-//             const orderFetched = await getOrderById(orderId);
+    const displayOrder = () => {
+        let total = 0;
 
-//             setOrder(orderFetched);
+        const [thisOrder, setThisOrder] = useState('')
 
-//         } catch (error) {
-//             console.error(error)
-//         }
-//     }
+        const fetchOrder = async () => {
+            try {
+                const order = await getOrderById(orderId);
 
-//     useEffect(() => {
-//         fetchOrder();
-//     }, []);
+                setThisOrder(order);
+            } catch (error) {
+                throw error;
+            };
+        };
 
-//     return <>
-//         <h1>This is the order with id: {orderId}</h1>
-//         {order ? <><p>Name of : { }</p>
-//             <p> description: { }</p>
-//             <p>Price: ${ }</p></>
-//             : ''}
-//     </>
-// };
+        useEffect(() => {
+            fetchOrder()
+        }, []);
 
-// export default Product;
+        return <div className='cart'>
+            <h1>This is a {thisOrder.status} order.</h1>
+            {thisOrder.products ? (
+                thisOrder.products.map(product => {
+                    total = product.totalProductPrice + total;
+                    return <div key={product.id}>
+                        <div className='product-card'><img className='thumbnail' src={product.imageURL} />
+                            <h3>{product.name}</h3>
+                            <p>Quantity: {product.quantity}</p>
+                            <p>Price: ${product.totalProductPrice * .01}</p>
+                        </div>
+                    </div>
+                })
+            ) : ''}
+            <br />
+            <h2>Total:  ${(total * .01).toFixed(2)}</h2>
+        </div>
+    }
 
+    return <div className='orders'>
+        {cart.id == orderId ? displayCart() : displayOrder()}
+    </div>
+};
