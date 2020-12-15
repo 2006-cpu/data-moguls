f278674b4ff56a4615573e55fa8d732485959d7c
dennisCastro <denniscastro@macbook-pro.lan>
2020-12-15 01:57:51 -0800

    121520 Design Tweaks

--- src/components/App.js
+++ src/components/App.js
@@ -64,8 +64,8 @@ export default function App() {
           <Route exact path='/'>
             <div className='home'>
               <div className='home-card'><h1>Welcome to KraftBier.com</h1>
-                <p>Modern U.S. craft beer history began in the 1960s. You may know part of the story, the increasing popularity of homebrewing beer in the 1970s and the rise of microbreweries in the 1990s. The sheer number of beer styles that make up the craft beer scene is exciting, but it can also be intimidating, and that’s okay!<br /><br /></p>
-                <p>With all of the different beer name and styles, it may be tough to remember what differentiates beers from one another. At KraftBier.com, we have compiled a collection of craft beers that have been favorites over the years, and have made it available direct to you, straight from the brewers themselves.</p><br /><br />
+                <p>Modern U.S. craft beer history began in the 1960's, rose in popularity in the 1970's and came to its own with the rise of microbreweries in the 1990's. The sheer number of beers that make up the craft beer scene is exciting, but it can also be intimidating, and that’s okay!<br /><br /></p>
+                <p>KraftBier.com is designed for the refined beer connoisseur, who appreciates specialty and unique beers from around the world. Since the beer market is saturated with thousands of craft beers, we've decided to narrow our product offerings to a select few, based on the most recent releases and customer recommendations. And, we rotate our product offerings every month, so that our selection stays fresh, relevant, and on the cutting edge.</p><br /><br />
                 <NavLink to='/products' className='button'>SHOP OUR PRODUCTS</NavLink></div>
             </div>
           </Route>
